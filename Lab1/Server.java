0d7df0865b005725135792c0ee17f0561a0297e9
emiwes <emiwes@kth.se>
2016-01-26 12:04:43 +0100

    Felhantering på user name.

--- Lab1/Server.java
+++ Lab1/Server.java
@@ -76,6 +76,25 @@ public class Server implements Runnable{
 			System.out.println("Source address: " + connection.socket.getInetAddress());
 		}
 	}
+
+	public synchronized ArrayList<String> getOtherUsers(String _sourceUser) {
+		ArrayList<String> users = new ArrayList<String>();
+		for(ServerConnection connection : connections){
+			if(!connection.username.equals(_sourceUser)){
+				users.add(connection.username);
+			}
+		}
+		return users;
+	}
+
+	public boolean isUsernameAvailability(String _sourceUser) {
+		for(ServerConnection connection : connections){
+			if(connection.username.equals(_sourceUser)){
+				return false;
+			}
+		}
+		return true;
+	}
 }
 
 class ServerConnection implements Runnable {
@@ -84,6 +103,7 @@ class ServerConnection implements Runnable {
 	Server server;
 	public String username = "";
 	public String id = "";
+	boolean loggedInFirstTime = true;
 
 	ServerConnection(Socket _socket, Server _server) {
 		socket = _socket;
@@ -98,37 +118,68 @@ class ServerConnection implements Runnable {
 			while (running) {
 				if (!socket.isClosed()) {
 					String message = input.readLine();
-					StringTokenizer tokens = new StringTokenizer(message, " ");
-					
-					if (tokens.hasMoreTokens()) {
-						String command = tokens.nextToken();
-						if (command.equals("@logout")) {
-							removeConnection();
-						} else if (command.equals("@username")) {
-							username = tokens.nextToken();
-						} else if (command.equals("@whisper")) {
-							String receiver = tokens.nextToken();
-							String privateMessage = tokens.nextToken("").trim();
-							server.broadcastTo(username + " whispers: " + privateMessage, receiver);
-						} else if (command.equals("@file")) {
-							String receiver = tokens.nextToken();
-							server.broadcastTo(command + " " + username, receiver);
-							System.out.println("File request sent to " + receiver);
-						} else if(command.equals("@fileanswer")){
-							String receiver = tokens.nextToken();
-							String answer = tokens.nextToken();
-							System.out.println("Fileanswer has been received. The answer to " + receiver + " is: " + answer);
-							server.broadcastTo(command + " " + username + " " + answer, receiver);
-						} else if(command.equals("@filesocketopen")){
-							String receiver = tokens.nextToken();
-							String sourceIP = tokens.nextToken();
-							String sourcePort = tokens.nextToken();
-							System.out.println("A socket has been opened. " + sourceIP + " and " + sourcePort + " is sent to: " + receiver);
-							server.broadcastTo(command + " " + username + " " + sourceIP + " " + sourcePort, receiver);
+					StringTokenizer tokens;
+					try{
+						tokens = new StringTokenizer(message, " ");
+						if (tokens.hasMoreTokens()) {
+							String command = tokens.nextToken();
+							if (command.equals("@logout")) {
+								server.broadcastAll("Server notification: " + username + " logged out.", id);
+								removeConnection();
+							} else if (command.equals("@username")) {
+								String tempUsername = tokens.nextToken();
+								if(server.isUsernameAvailability(tempUsername)){
+									if(loggedInFirstTime){
+										server.broadcastAll("Server notification: " + tempUsername + " logged in.", id);
+										loggedInFirstTime = false;
+									} else{
+										server.broadcastAll("Server notification: " + username + " changed name to " + tempUsername, id);
+									}
+									username = tempUsername;
+								}else{
+									if(loggedInFirstTime){
+										server.broadcastAll("Server notification: " + username + " logged in.", id);
+										loggedInFirstTime = false;
+									} 
+									server.broadcastTo("Server notification: Username not available!", username);
+								}
+							} else if (command.equals("@users")){
+								ArrayList<String> users = server.getOtherUsers(username);
+								String msg = "Users in chat: ";
+								for(String user : users){
+									msg += user + ", ";
+								}
+								server.broadcastTo(msg, username);
+								
+							} else if (command.equals("@whisper")) {
+								String receiver = tokens.nextToken();
+								String privateMessage = tokens.nextToken("").trim();
+								server.broadcastTo(username + " whispers: " + privateMessage, receiver);
+							} else if (command.equals("@file")) {
+								String receiver = tokens.nextToken();
+								String filePath = tokens.nextToken();
+								String size = tokens.nextToken(); 
+								server.broadcastTo(command + " " + username + " " + filePath + " " + size, receiver);
+								System.out.println("File request sent to " + receiver);
+							} else if(command.equals("@fileanswer")){
+								String receiver = tokens.nextToken();
+								String answer = tokens.nextToken();
+								System.out.println("Fileanswer has been received. The answer to " + receiver + " is: " + answer);
+								server.broadcastTo(command + " " + username + " " + answer, receiver);
+							} else if(command.equals("@filesocketopen")){
+								String receiver = tokens.nextToken();
+								String sourceIP = tokens.nextToken();
+								String sourcePort = tokens.nextToken();
+								System.out.println("A socket has been opened. " + sourceIP + " and " + sourcePort + " is sent to: " + receiver);
+								server.broadcastTo(command + " " + username + " " + sourceIP + " " + sourcePort, receiver);
+							}
+							else {
+								server.broadcastAll(username + ": " + message, id);
+							}
 						}
-						else {
-							server.broadcastAll(username + ": " + message, id);
-						}
+					}catch(NullPointerException e){
+						System.err.println(username + " has disconnected abruptly.");
+						removeConnection();
 					}
 				} else {
 					removeConnection();
