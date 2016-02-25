7d4fe17a6a62ab81f207c2b4600822da12db5ba4
Arvid Sätterkvist <arvidsat@gmail.com>
2016-02-25 08:16:44 +0100

    Updated bostad.sql

--- Lab4/bostad.sql
+++ Lab4/bostad.sql
@@ -29,5 +29,31 @@ insert into residence values ('Uppsala','Villa','Kungsängsvägen 1',130,5,10000
 
 insert into residence values ('Uppsala','Villa','Kungsängsvägen 2',160,6,1000000,3456);
 
+insert into residence values ('Stockholm','Hyresrätt','Observatoriegatan 3',165,5,0,12483);
+
+insert into residence values ('Stockholm','Bostadsrätt','Norrbackagatan 44',28,1,2800000,1200);
+
+insert into residence values ('Kiruna','Villa','Gruvgatan 4',300,8,300000,200);
+
+insert into residence values ('Kiruna','Villa','Schaktsvängen 23',49,3,500000,1500);
+
+insert into residence values ('Kiruna','Bostadsrätt','Dåndimpsfjället 89',10,1,20000,0);
+
+insert into residence values ('Malmö','Bostadsrätt','Rosengårdsgatan 157',28,1,2800000,1200);
+
+insert into residence values ('Malmö','Hyresrätt','Norrbackagatan 33',28,1,2800000,1200);
+
+insert into residence values ('Malmö','Radhus','Radhuskröken 4',28,3,7000000,12300);
+
+insert into residence values ('Malmö','Radhus','Radhuskröken 44',28,1,2800000,1200);
+
+insert into residence values ('Norrköping','Bostadsrätt','Kulvägen 8',47,2,3200000,1400);
+
+insert into residence values ('Norrköping','Bostadsrätt','Första Gatan 1',630,6,9500000,1200);
+
+insert into residence values ('Norrköping','Villa','Villavillekullavägen 1000',28,1,666,1200);
+
+insert into residence values ('Norrköping','Bostadsrätt','bogatan 4',28,4,720000,1337);
+
 
 SELECT * FROM residence
